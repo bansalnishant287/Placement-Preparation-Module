284b4d7f7b3ede832d8ceb67325c430749dd610e
Nishant Bansal <nishant.1923it1002@kiet.edu>
2022-07-05 23:27:18 +0530

    Update Find the Duplicate Number.java

--- Day2: Arrays Part-II/Find the Duplicate Number.java
+++ Day2: Arrays Part-II/Find the Duplicate Number.java
@@ -1,1 +1,32 @@
-/*/
+/*
+Link: "https://leetcode.com/problems/find-the-duplicate-number/"
+287. Find the Duplicate Number
+Medium
+
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and uses only constant extra space.
+
+Example 1:
+
+Input: nums = [1,3,4,2,2]
+Output: 2
+*/
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0], fast = nums[0];
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        fast = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+}
